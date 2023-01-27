67ebf83411e8d0ab6e29e65251fa1699c993cb1c
Lukas <lukas.ldr@gmail.com>
2023-01-27 11:44:45 +0100

    :truck: Created the Header element

---
+++ src/components/elements/Header.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+
+import type { ReactElement, ReactNode } from 'react';
+
+type HeaderType = 1 | 2 | 3 | 4;
+
+interface IProps {
+  type: HeaderType;
+  className: string;
+  children?: ReactNode;
+}
+
+const Header = ({ type, className, children }: IProps): ReactElement => {
+  switch (type) {
+    case 1:
+      return (
+        <h1 className={clsx('text-[64px] lg:text-[78px]', className)}>
+          {children}
+        </h1>
+      );
+
+    case 2:
+      return (
+        <h2 className={clsx('text-[32px] lg:text-[38px]', className)}>
+          {children}
+        </h2>
+      );
+
+    case 3:
+      return <h3 className={clsx('text-[26px]', className)}>{children}</h3>;
+
+    default:
+      return <h4 className={clsx('text-[22px]')}>{children}</h4>;
+  }
+};
+
+export default Header;
