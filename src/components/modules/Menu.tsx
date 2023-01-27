eb0925ee01a3f7b735302b4c04e6ac4f81f3d9eb
Lukas <lukas.ldr@gmail.com>
2023-01-27 12:05:36 +0100

    :truck: Added the Menu module

---
+++ src/components/modules/Menu.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+
+import { useRouter } from 'next/router';
+
+import type { ReactElement } from 'react';
+
+import MENU from '@config/menu.json';
+
+interface IProps {
+  className?: string;
+}
+
+const Menu = ({ className }: IProps): ReactElement => {
+  const router = useRouter();
+
+  return (
+    <nav className={clsx([className])}>
+      <ul className="flex flex-row gap-24">
+        {MENU.map((page) => (
+          <p
+            className={clsx(
+              'text-ternary font-medium uppercase',
+              !router.pathname.startsWith(page.path) && 'opacity-50'
+            )}
+          >
+            {page.name}
+          </p>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
