1523be62aeee26922c597e9f652b3d94d3af7a9a
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-01-28 14:19:07 +0100

    :truck: Build the second section of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,14 @@ import Separator from '@element/Separator';
 import Header from '@element/Header';
 
 import menu from '../../public/icons/menu.svg';
+import clsx from 'clsx';
 
 const Home = (): ReactElement => {
   const openMenu = () => {};
 
   return (
     <>
-      <section className="flex flex-col items-center h-screen p-8">
+      <section className="flex flex-col items-center min-h-screen p-8">
         <AreaButton onClick={openMenu} className="self-end">
           <Image src={menu} alt="Menu Icon" height={32} width={32} />
         </AreaButton>
@@ -35,6 +36,118 @@ const Home = (): ReactElement => {
         <AnchorButton href="/contact" className="mt-16">
           Se rencontrer
         </AnchorButton>
+      </section>
+
+      <section className="flex flex-col p-8" id="accompaniement">
+        <Header type={2} className="font-mono text-primary">
+          Votre accompagnement
+        </Header>
+        <Separator size="normal" />
+
+        <article className="mt-14 pb-8">
+          <Header type={4}>Entretiens préalable</Header>
+
+          <p className="text-primaryLight w-[80%]">
+            Une première rencontre afin d’accorder vos attentes avec la solution
+            que je vous fournirai.
+          </p>
+
+          <div
+            className={clsx([
+              'absolute top-1/3 left-[80%]',
+              'transform -translate-x-[2px] -translate-y-[1.375px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute bottom-[5%] right-[5%]',
+              'transform translate-x-[1.375px] translate-y-[2px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute right-[5%] bottom-[5%] left-[80%] top-1/3',
+              'border-t-[1.25px] border-r-[1.25px] border-ternary rounded-tr-xl'
+            ])}
+          ></div>
+        </article>
+
+        <article className="mt-2 pb-8">
+          <Header type={4}>Le devis</Header>
+
+          <p className="text-primaryLight text-right w-[80%]">
+            Création d’un cahier des charges en accord avec vos souhaits afin de
+            remplir complètement vos attentes.
+          </p>
+
+          <div
+            className={clsx([
+              'absolute top-1/3 right-[80%]',
+              'transform translate-x-[2px] -translate-y-[1.375px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute bottom-[5%] left-[5%]',
+              'transform -translate-x-[1.375px] translate-y-[2px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute right-[80%] bottom-[5%] left-[5%] top-1/3',
+              'border-t-[1.25px] border-l-[1.25px] border-ternary rounded-tl-xl'
+            ])}
+          ></div>
+        </article>
+
+        <article className="my-2">
+          <Header type={4}>La conception</Header>
+
+          <p className="text-primaryLight w-[80%]">
+            Développement de votre solution avec un suivi permanent de
+            l’avancement pour éviter les erreurs.
+          </p>
+        </article>
+
+        <article className="pt-8">
+          <Header type={4}>Le suivi</Header>
+
+          <p className="text-primaryLight text-right w-[80%]">
+            Vous avez besoin de révisions où de modifications après la
+            finalisation du développement. Le suivi est la pour ça.
+          </p>
+
+          <div
+            className={clsx([
+              'absolute top-[5%] left-[5%]',
+              'transform -translate-y-[2px] -translate-x-[1.375px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute bottom-1/3 right-[80%]',
+              'transform translate-y-[1.375px] translate-x-[2px]',
+              'h-1 w-1 bg-ternary rounded-full'
+            ])}
+          ></div>
+
+          <div
+            className={clsx([
+              'absolute right-[80%] top-[5%] left-[5%] bottom-1/3',
+              'border-b-[1.25px] border-l-[1.25px] border-ternary rounded-bl-xl'
+            ])}
+          ></div>
+        </article>
       </section>
     </>
   );
