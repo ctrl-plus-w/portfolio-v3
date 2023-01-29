7ff58b8f11aa92e203cdf861159cc84e2e15bb6b
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-01-29 18:42:21 +0100

    :truck: Added the curves to the pages

--- src/components/modules/Layout.tsx
+++ src/components/modules/Layout.tsx
@@ -1,13 +1,19 @@
-// import { useRef } from 'react';
-
 import type { ReactElement, ReactNode } from 'react';
 
 import Footer from '@module/Footer';
 import Menu from '@module/Menu';
+import Curve from '@class/Curve';
 
-// import useAnimatedCurves from '@hook/useAnimatedCurves';
-
-// import { Curve1, Curve2, Curve3, Curve4, Curve5, Curve6 } from '@config/curves';
+import {
+  Curve1,
+  Curve2,
+  Curve3,
+  Curve4,
+  Curve5,
+  Curve6,
+  Curve7,
+  Curve8
+} from '@config/curves';
 
 interface IProps {
   className?: string;
@@ -15,19 +21,25 @@ interface IProps {
 }
 
 const Layout = ({ children }: IProps): ReactElement => {
-  // const animationContainerRef1 = useAnimatedCurves<HTMLDivElement>(
-  //   Curve1,
-  //   Curve2
-  // );
-
   return (
     <>
-      {/* <div className="absolute w-full h-full overflow-hidden">
-        <div
-          className="absolute top-0 left-0 lg:transform lg:scale-150 lg:origin-top-left"
-          ref={animationContainerRef1}
-        ></div>
-      </div> */}
+      <div className="absolute w-full h-full -z-10">
+        <div className="absolute top-0 left-0 lg:transform lg:scale-150 lg:origin-top-left">
+          {Curve.reactCombinePaths(Curve1, Curve2)}
+        </div>
+
+        <div className="absolute top-[30vh] right-0 lg:transform lg:scale-150 lg:origin-top-right">
+          {Curve.reactCombinePaths(Curve3, Curve4)}
+        </div>
+
+        <div className="absolute top-[110vh] left-0 lg:transform lg:scale-150 lg:origin-top-left">
+          {Curve.reactCombinePaths(Curve5, Curve6)}
+        </div>
+
+        <div className="absolute bottom-64 right-0 lg:transform lg:scale-150 lg:origin-bottom-right">
+          {Curve.reactCombinePaths(Curve7, Curve8)}
+        </div>
+      </div>
 
       <Menu />
       <main>{children}</main>
