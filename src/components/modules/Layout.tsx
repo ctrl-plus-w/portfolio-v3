22c91e771c824ea49672d1213361fd47355ef8c9
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-01-29 18:01:47 +0100

    :truck: Commented

--- src/components/modules/Layout.tsx
+++ src/components/modules/Layout.tsx
@@ -1,7 +1,13 @@
+// import { useRef } from 'react';
+
 import type { ReactElement, ReactNode } from 'react';
 
 import Footer from '@module/Footer';
 import Menu from '@module/Menu';
+
+// import useAnimatedCurves from '@hook/useAnimatedCurves';
+
+// import { Curve1, Curve2, Curve3, Curve4, Curve5, Curve6 } from '@config/curves';
 
 interface IProps {
   className?: string;
@@ -9,8 +15,20 @@ interface IProps {
 }
 
 const Layout = ({ children }: IProps): ReactElement => {
+  // const animationContainerRef1 = useAnimatedCurves<HTMLDivElement>(
+  //   Curve1,
+  //   Curve2
+  // );
+
   return (
     <>
+      {/* <div className="absolute w-full h-full overflow-hidden">
+        <div
+          className="absolute top-0 left-0 lg:transform lg:scale-150 lg:origin-top-left"
+          ref={animationContainerRef1}
+        ></div>
+      </div> */}
+
       <Menu />
       <main>{children}</main>
       <Footer />
