6e88336b7453a9daba8260f5d89a960f2b3269a9
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-01-29 18:01:20 +0100

    :gear: Added the curves data config

---
+++ src/config/curves.ts
@@ -0,0 +1,83 @@
+import Vector from '@class/Vector';
+import Color from '@class/Color';
+import Curve from '@class/Curve';
+
+const CURVE_COLOR = new Color(196, 132, 14, 0.3);
+
+export const Curve1 = new Curve(
+  538,
+  408,
+  new Vector(537, 0),
+  new Vector(476, 36),
+  new Vector(400, 73),
+  new Vector(308, 106),
+  new Vector(132, 170),
+  new Vector(41, 296),
+  new Vector(0, 399),
+  CURVE_COLOR
+);
+
+export const Curve2 = new Curve(
+  538,
+  408,
+  new Vector(463, 0),
+  new Vector(370, 56),
+  new Vector(240, 165),
+  new Vector(163, 255),
+  new Vector(103, 324),
+  new Vector(42, 375),
+  new Vector(0, 406),
+  CURVE_COLOR
+);
+
+export const Curve3 = new Curve(
+  321,
+  875,
+  new Vector(321, 0),
+  new Vector(176, 19),
+  new Vector(-47, 91),
+  new Vector(10, 268),
+  new Vector(60, 422),
+  new Vector(217, 492),
+  new Vector(321, 604),
+  CURVE_COLOR
+);
+
+export const Curve4 = new Curve(
+  321,
+  875,
+  new Vector(321, 23),
+  new Vector(174, -56),
+  new Vector(34, 91),
+  new Vector(56, 275),
+  new Vector(90, 516),
+  new Vector(234, 731),
+  new Vector(321, 874),
+  CURVE_COLOR
+);
+
+export const Curve5 = new Curve(
+  181,
+  925,
+  new Vector(0, 77),
+  new Vector(39, 116),
+  new Vector(96, 177),
+  new Vector(146, 254),
+  new Vector(223, 374),
+  new Vector(166, 625),
+  new Vector(0, 800),
+  CURVE_COLOR
+);
+
+export const Curve6 = new Curve(
+  181,
+  925,
+  new Vector(0, 0),
+  new Vector(57, 10),
+  new Vector(136, 292),
+  new Vector(139, 429),
+  new Vector(142, 561),
+  new Vector(62, 871),
+  new Vector(0, 925),
+  CURVE_COLOR
+);
