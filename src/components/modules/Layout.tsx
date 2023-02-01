f043f3de4de26811cad5e599aef8858f338f36e1
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-01 18:12:50 +0100

    :art: Made the loading animations

--- src/components/modules/Layout.tsx
+++ src/components/modules/Layout.tsx
@@ -1,7 +1,12 @@
+import { useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
+
 import type { ReactElement, ReactNode } from 'react';
 
 import Footer from '@module/Footer';
 import Menu from '@module/Menu';
+
+import Loader from '@element/Loader';
 
 import useBreakpoints from '@hook/useBreakpoints';
 
@@ -28,25 +33,95 @@ interface IProps {
 const Layout = ({ children }: IProps): ReactElement => {
   const { greaterThan } = useBreakpoints();
 
+  useLayoutEffect(() => {
+    const timeline = gsap.timeline();
+
+    timeline.to('#loader', {
+      opacity: 0,
+      pointerEvents: 'none',
+      delay: 0.5
+    });
+
+    timeline
+      .fromTo(
+        '#line1 svg',
+        { strokeDasharray: '2000px', strokeDashoffset: '2000px' },
+        {
+          strokeDashoffset: 0,
+          duration: 2
+        }
+      )
+      .fromTo(
+        '#line2 svg',
+        { strokeDasharray: '2000px', strokeDashoffset: '-2000px' },
+        {
+          strokeDashoffset: 0,
+          duration: 1
+        },
+        '<'
+      )
+      .from(
+        '.separator',
+        {
+          width: 0
+        },
+        '<'
+      )
+
+      .from(
+        'h1, h2, p, a, button',
+        {
+          opacity: 0,
+          y: '1rem'
+        },
+        '<'
+      )
+      .from(
+        '.menu-icon',
+        {
+          opacity: 0
+        },
+        ''
+      );
+
+    return () => {
+      timeline.revert();
+    };
+  }, []);
+
   return (
     <>
+      <Loader />
+
       <div className="absolute inset-0 h-full -z-10 overflow-hidden">
-        <div className="absolute top-0 -left-48 md:left-0 md:transform md:scale-150 md:origin-top-left">
+        <div
+          className="line absolute top-0 -left-48 md:left-0 md:transform md:scale-150 md:origin-top-left"
+          id="line1"
+        >
           {Curve.reactCombinePaths(Curve1, Curve2)}
         </div>
 
-        <div className="absolute top-[30vh] -right-48 md:right-0 md:transform md:scale-150 md:origin-top-right">
+        <div
+          className="line absolute top-[30vh] -right-48 md:right-0 md:transform md:scale-150 md:origin-top-right"
+          id="line2"
+        >
           {Curve.reactCombinePaths(Curve3, Curve4)}
         </div>
 
         {renderIf(
           !greaterThan('lg'),
           <>
-            <div className="absolute top-[110vh] -left-18 md:left-0 md:transform md:scale-150 md:origin-top-left">
+            <div
+              className="line absolute top-[110vh] -left-18 md:left-0 md:transform md:scale-150 md:origin-top-left"
+              id="line3"
+            >
               {Curve.reactCombinePaths(Curve5, Curve6)}
             </div>
 
-            <div className="absolute bottom-32 right-0 md:transform md:scale-150 md:origin-bottom-right">
+            <div
+              className="line absolute bottom-32 right-0 md:transform md:scale-150 md:origin-bottom-right"
+              id="line4"
+            >
               {Curve.reactCombinePaths(Curve7, Curve8)}
             </div>
           </>
