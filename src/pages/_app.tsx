f524330f6bbc54a8d5798140b718f20cdf702821
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-07 12:50:04 +0100

    :gear: Added Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,44 @@ import '@style/globals.css';
 import type { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 
+import Script from 'next/script';
+
 import Layout from '@module/Layout';
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-EETSRYKHYP"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-EETSRYKHYP');
+// </script>
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-EETSRYKHYP"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-EETSRYKHYP');
+          `
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
